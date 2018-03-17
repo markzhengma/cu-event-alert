dd8bfb8f30468f1f15e6b746cf9bea7d6ca96539
Mark Ma <mazheng1022@hotmail.com>
2018-03-17 15:23:41 -0400

    manage date input and output

--- client/src/components/SingleEvent.jsx
+++ client/src/components/SingleEvent.jsx
@@ -6,6 +6,14 @@ import axios from 'axios';
 class SingleEvent extends Component {
     constructor() {
         super();
+        this.state = {
+            date: null,
+        }
+    };
+
+    componentWillMount(){
+        this.state.date = new Date(this.props.selectedEvent.event_time)
+        console.log(this.state.date);
     };
 
     render() {
@@ -14,9 +22,9 @@ class SingleEvent extends Component {
                 <h1>{this.props.selectedEvent.event_name}</h1>
                 <p>{this.props.selectedEvent.likes} Likes</p>
                 <p><b>Event Type: </b>{this.props.selectedEvent.event_type}</p>
-                <p><b>Start Time: </b>{this.props.selectedEvent.event_time}</p>
+                <p><b>Start Time: </b>{new Date(this.props.selectedEvent.event_time).toDateString()}</p>
                 <p><b>Location: </b>{this.props.selectedEvent.event_location} ({this.props.selectedEvent.event_location_detail})</p>
-                <p><b>Fees: {this.props.selectedEvent.event_fee}</b></p>
+                <p><b>Fees: ${this.props.selectedEvent.event_fee}</b></p>
                 <p><b>Description: </b>{this.props.selectedEvent.event_desc}</p>
                 {this.props.user ? 
                     this.props.selectedEvent.user_id == this.props.user.id ?
