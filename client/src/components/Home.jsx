98fdb3b8706e2ea97b36bb799f8758ceecdfa151
Mark Ma <mazheng1022@hotmail.com>
2018-03-17 23:13:51 -0400

    handle EventHighlight errors

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,9 +11,12 @@ const Home = (props) => {
          <div className = 'home-content'>
             <SearchBox/>
             <EventTag/>
-            <EventHighlight eventData = {props.eventData}
-                            selectEventHandler = {props.selectEventHandler}
-                            />
+            {props.eventData ? 
+                <EventHighlight eventData = {props.eventData}
+                                selectEventHandler = {props.selectEventHandler}
+                                />
+                : ""
+            }
             <EventRanking eventData = {props.eventData}/>
             {props.auth ? <EventRecommend user = {props.user}/> : ""}
         </div>
